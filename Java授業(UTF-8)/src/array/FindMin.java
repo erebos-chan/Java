1b1c4656e6a04aa95ab90341feececc847bd363a
erebos-chan <wataru.new@gmail.com>
2016-06-08 10:48:24 +0900

    2016/06/08 10:46

--- Java授業(UTF-8)/src/array/FindMin.java
+++ Java授業(UTF-8)/src/array/FindMin.java
@@ -1,17 +1,18 @@
 package array;
+
 import java.util.Scanner;
 
 public class FindMin {
 
 	public static void main(String[] args) {
-		//スキャナの準備
+		// スキャナの準備
 		Scanner scan = new Scanner(System.in);
-		
-		//配列＆変数の宣言
+
+		// 配列＆変数の宣言
 		int num[] = new int[5];
 		int min;
-		
-		//プロンプト＆ユーザ入力
+
+		// プロンプト＆ユーザ入力
 		System.out.println("入力した数値の最小値を求めます。");
 		for (int i = 0; i < 5; i++) {
 			System.out.print("整数を入力");
@@ -19,26 +20,26 @@ public class FindMin {
 			System.out.print("番目>>");
 			num[i] = scan.nextInt();
 		}
-		//メソッド呼び出し
+		// メソッド呼び出し
 		min = findMin(num);
-		
-		//表示
-		System.out.printf("最大値は『%d』",min);
-		
-		//スキャナCLose
+
+		// 表示
+		System.out.printf("最小値は『%d』", min);
+
+		// スキャナCLose
 		scan.close();
 
 	}
-	static int findMin(int num[]){
-		int min = 0;
-		for(int k = 0;k < 5;k++){
-			if(num[k] < min){
+
+	static int findMin(int num[]) {
+		int min = 99999999;
+		for (int k = 0; k < 5; k++) {
+			if (num[k] < min) {
 				min = num[k];
 			}
 		}
-		
-		
-	return min;
+
+		return min;
 	}
 
 }
---
+++ Test/src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int sum = 100;
+		
+		System.out.println("合計は" + sum + "です。");
+		System.out.printf("合計は%dです。",sum);
+		
+}}
